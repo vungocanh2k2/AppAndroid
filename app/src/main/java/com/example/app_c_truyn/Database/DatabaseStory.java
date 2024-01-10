1ed050e2f696ae0729f05dd3eaa2b048e0788fe8
hoang <vehoang2k2@gmail.com>
2024-01-10 09:12:51 +0700

    Start app - DATABASE

--- app/src/main/java/com/example/app_c_truyn/Database/DatabaseStory.java
+++ app/src/main/java/com/example/app_c_truyn/Database/DatabaseStory.java
@@ -1,5 +1,44 @@
 package com.example.app_c_truyn.Database;
 
-public class DatabaseStory {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
+import androidx.annotation.Nullable;
+
+public class DatabaseStory extends SQLiteOpenHelper {
+    private static String DATABASE_NAME = "doctruyen";
+    private static String TABLE_TAIKHOAN = "taikhoan";
+    private static String ID_TAI_KHOAN = "idtaikhoan";
+    private static String TEN_TAI_KHOAN = "tentaikhoan";
+    private static String MAT_KHAU = "matkhau";
+    private static String PHAN_QUYEN = "phanquyen";
+    private static String EMAIL = "email";
+    private static int VERSION = 1;
+
+    private static String TABLE_STORY = "story";
+    private static String ID_STORY = "id_story";
+    private static String NAME_STORY = "title";
+    private static String CONTENT = "content";
+    private static String IMAGE = "image";
+
+    private String CREATE_TABLE_STORY = "CREATE TABLE "+ TABLE_STORY +" ( "+ID_STORY+" integer primary key AUTOINCREMENT, "
+            +NAME_STORY +" TEXT UNIQUE, "
+            +CONTENT+" TEXT, "
+            +IMAGE+" TEXT, "+ID_TAI_KHOAN+" INTEGER , FOREIGN KEY ( "+ ID_TAI_KHOAN +" ) REFERENCES "+
+            TABLE_TAIKHOAN+"("+ID_TAI_KHOAN+"))";
+
+    public DatabaseStory(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_STORY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
--- app/src/main/java/com/example/app_c_truyn/Model/Story.java
+++ app/src/main/java/com/example/app_c_truyn/Model/Story.java
@@ -1,5 +1,64 @@
 package com.example.app_c_truyn.Model;
 
 public class Story {
+    private int ID;
+    private String NameStory;
+    private String Content;
+    private String Image;
+    private int ID_TK;
 
+    public Story(String nameStory, String content, String image, int ID_TK) {
+        NameStory = nameStory;
+        Content = content;
+        Image = image;
+        this.ID_TK = ID_TK;
+    }
+
+    public Story(int ID, String nameStory, String content, String image, int ID_TK) {
+        this.ID = ID;
+        NameStory = nameStory;
+        Content = content;
+        Image = image;
+        this.ID_TK = ID_TK;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getNameStory() {
+        return NameStory;
+    }
+
+    public void setNameStory(String nameStory) {
+        NameStory = nameStory;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public String getImage() {
+        return Image;
+    }
+
+    public void setImage(String image) {
+        Image = image;
+    }
+
+    public int getID_TK() {
+        return ID_TK;
+    }
+
+    public void setID_TK(int ID_TK) {
+        this.ID_TK = ID_TK;
+    }
 }
